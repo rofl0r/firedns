0ae83311d6c0c26f385995572cf672ae96d558aa
rofl0r <retnyg@gmx.net>
2011-09-04 18:14:55 +0200

    take care about potential padding

--- src/firedns/firedns_freeconn.c
+++ src/firedns/firedns_freeconn.c
@@ -2,6 +2,8 @@
 #include <string.h>
 
 void firedns_freeconn(firedns_state* self, struct s_connection* conn) {
-	size_t index = (conn - &self->connections[0]) / sizeof(struct s_connection);
+	size_t index = (conn - &self->connections[0]) / 
+		(&self->connections[1] - &self->connections[0]);
+		// ^ use the exact distance including padding...
 	self->conn_used[index] = 0;
 }
