7a2fb948551c1c252c551726ceeed782a2a58d34
rofl0r <retnyg@gmx.net>
2011-11-10 06:05:52 +0100

    ...

--- src/firedns_getmx.c
+++ src/firedns_getmx.c
@@ -5,15 +5,18 @@ int firedns_getmx(firedns_state* self, const char* name) {
 	struct s_connection* s;
 	int l;
 	l = firedns_build_query_payload(name, FDNS_QRY_MX, 1, (unsigned char*) &h.payload);
-	if (l == -1)
+	if (l == -1) {
 		return -1;
+	}
 	s = firedns_add_query(self, &h);
-	if (s == NULL)
+	if (s == NULL) {
 		return -1;
+	}
 	s->class = 1;
 	s->type = FDNS_QRY_MX;
-	if (firedns_send_requests(self, &h, s, l) == -1)
+	if (firedns_send_requests(self, &h, s, l) == -1) {
 		return -1;
+	}
 	return s->fd;
 }
 
